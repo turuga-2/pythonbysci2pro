ad5f4f54e5758c3e0c9236cf3b950604ce9fec08
T <81047234+turuga-2@users.noreply.github.com>
2021-06-09 14:45:19 +0300

    exceptions notes

--- week3/exceptions.py
+++ week3/exceptions.py
@@ -2,6 +2,32 @@ import sys
 
 
 def main():
+    # index error
+    exe0 = list(range(10))
+
+    # print(exe0[11])
+
+    # catching exceptions
+    try:
+        v = exe0[11]
+    except IndexError as ex:
+        print(f"WARNING: {ex}; THE LIST YOU PROVIDED WAS TOO SHORT!!")
+        v = 0
+    print(f"v = {v}")
+    # catching multiple exceptions
+    try:
+        v = "s" + exe0[4]
+    except TypeError as le:
+        print(f"WARNING: {le}; not the right type")
+        v = "s0"
+    print(f"v = {v}")
+    # assertion
+    age = int(input("Your age"))
+    try:
+        assert 0 < age < 120
+    except AssertionError:
+        print(f"error: invalid age {age}")
+
     return 0
 
 
