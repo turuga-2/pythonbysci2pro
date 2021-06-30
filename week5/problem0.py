87f281a6d9ae65e098d23744ebf37e9e8d41caf8
T <81047234+turuga-2@users.noreply.github.com>
2021-06-30 13:15:13 +0300

    correct code

--- week5/problem0.py
+++ week5/problem0.py
@@ -3,18 +3,26 @@ import random
 
 
 def main():
-    l = [random.sample(range(0, 100), 100)]
+    l = list(random.sample(range(0, 100), 100))
     print(l)
 
+    # new_1 = [sum(l[0: x + 1]) for x in range(0, len(l))]
     new_1 = []
     n = 0
     for x in range(0, len(l)):
         n += l[x]
         new_1.append(n)
-
     print(new_1)
 
-    # new_1 =list(new_1[n-1] = list1[0] + list1[1])
+    new_2 = list(q+r for q, r in zip(l, l[1:]))
+    new_2.append(0)
+    print(new_2)
+    print(len(new_2))
+
+    new_3 = list(a+ b+ c for a, b, c in zip(l, l[2:], l[4:]))
+    new_3.append(0)
+    print(new_3)
+    print(len(new_3))
 
     return 0
 
