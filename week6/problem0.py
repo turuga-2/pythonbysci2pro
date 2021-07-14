f0485ee0d5fd783a081971eaaf9738e1a4a05e92
Paul K. Korir <31738053+paulkorir@users.noreply.github.com>
2021-07-14 06:39:43 +0100

    Update problem0.py

--- week6/problem0.py
+++ week6/problem0.py
@@ -3,11 +3,14 @@ import math
 
 
 def sum_of_list(example_list):
+    # todo: add a docstring; see class notes on how to do this
+    # fixme: do not use sum()
     the_sum = sum(example_list)
     print(f'the sum is:{the_sum}')
     return the_sum
 
 def main():
+    # todo: give your list a better name
     example_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
     print(example_list)
     sum_of_list(example_list)
