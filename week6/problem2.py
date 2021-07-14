8a757b81b03c4c2cc3835f6310d89c06545f99e3
Paul K. Korir <31738053+paulkorir@users.noreply.github.com>
2021-07-14 06:46:29 +0100

    Update problem2.py

--- week6/problem2.py
+++ week6/problem2.py
@@ -1,9 +1,14 @@
 import sys
 
 
+# todo: give the function a better name; this name suggests that it returns a palindrom
+# remember what we said about naming functions
 def palindrome():
+    # todo: add a docstring; see class notes on how to do this
     test_string = input("please input the string to be tested")
+    # excellent that you have considered case
     test_string = test_string.casefold()
+    # fixme: avoid using builtin functions for this problem
     reverse_test_string = reversed(test_string)
     if list(test_string) == list(reverse_test_string):
         print(f"The string {test_string} is a palindrome")
